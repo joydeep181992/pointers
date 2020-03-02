679e5d93a0142af6eb1119fe8d27cd74014f134e
joydeep181992 <joydeep181992@gmail.com>
2020-03-01 22:32:22 -0500

    Passing array to a fucntion

--- arryFuncPass.c
+++ arryFuncPass.c
@@ -1,3 +1,4 @@
+/*Not a proper function. Need to fix the bug*/
 #include<stdio.h>
 int length(int *a) {
 	int len = sizeof(a)/sizeof(a[0]);
@@ -7,7 +8,7 @@ int length(int *a) {
 int main() {
 	int a[] = {1,2,3,4,5};
 //	DoubleTheValue(a);
-	int a = length(a);
-	printf("The length is %d", a);
+	int len = length(a);
+	printf("The length is %d", len);
 	return 0;
 }
